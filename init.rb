495987fb21d2a567a2501a8a6e22644c9c8bcb3a
Derek <dreynolds@trulia.com>
2012-12-02 01:33:00 -0800

    Now rendering a list of movies at each venue.

--- init.rb
+++ init.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'foursquare2'
 
 get '/' do
   client = Foursquare2::Client.new(:client_id => 'AJ5B4KWNYX5MCTWPQJYA5WCTICID404HBRX04BDMM1SDSMNI', :client_secret => 'GPKCG2YQNVTFPJN5OGE5JMCGQ1WOVDKGSPJCYDRPTQTZYOBJ')
-  @result = client.search_venues(:near => 'San Francisco, CA', :categoryId => '4bf58dd8d48988d17f941735')
-  @venues = @result["groups"].first["items"]
+  @venues_result = client.search_venues(:near => 'San Francisco, CA', :categoryId => '4bf58dd8d48988d17f941735')
+  @venues = @venues_result["groups"].first["items"]
+
+ # @events = client.venue_events(@venues.first["id"])
+  # 4dfb90c6bd413dd705e8f897 // events category id
+
   erb :index
+end
+
+get '/venue/:venue_id/movies' do
+  client = Foursquare2::Client.new(:client_id => 'AJ5B4KWNYX5MCTWPQJYA5WCTICID404HBRX04BDMM1SDSMNI', :client_secret => 'GPKCG2YQNVTFPJN5OGE5JMCGQ1WOVDKGSPJCYDRPTQTZYOBJ')
+
+  @events_result = client.venue_events(params["venue_id"])
+
+  json @events_result["events"]["items"]
 end
