ce129edcf80f2daee9f6c9f5d6aa18846ed59355
Derek <derekr@me.com>
2013-01-26 23:24:32 -0800

    Setup to use foreman and env vars for config. Locked in ruby version.

--- init.rb
+++ init.rb
@@ -3,12 +3,20 @@ require 'sinatra/json'
 require 'foursquare2'
 
 get '/' do
-  client = Foursquare2::Client.new(:client_id => 'AJ5B4KWNYX5MCTWPQJYA5WCTICID404HBRX04BDMM1SDSMNI', :client_secret => 'GPKCG2YQNVTFPJN5OGE5JMCGQ1WOVDKGSPJCYDRPTQTZYOBJ')
-  @venues_result = client.search_venues(:near => 'San Francisco, CA', :categoryId => '4bf58dd8d48988d17f941735')
+  puts ENV['FOURSQUARE_ID']
+  puts ENV['FOURSQUARE_SECRET']
+  
+  client = Foursquare2::Client.new(
+    :client_id => ENV['FOURSQUARE_ID'],
+    :client_secret => ENV['FOURSQUARE_SECRET']
+  )
+    
+  @venues_result = client.search_venues(
+    :near => 'San Francisco, CA',
+    :categoryId => '4bf58dd8d48988d17f941735'
+  )
+    
   @venues = @venues_result["groups"].first["items"]
-
- # @events = client.venue_events(@venues.first["id"])
-  # 4dfb90c6bd413dd705e8f897 // events category id
 
   erb :index
 end
