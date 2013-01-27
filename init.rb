df02fd1a45aafcb748ee55b51dd7b27470e2743c
Derek <derekr@me.com>
2013-01-27 00:07:47 -0800

    Moved venue retrieval to the client and added /venues endpoint

--- init.rb
+++ init.rb
@@ -2,28 +2,31 @@ require 'sinatra'
 require 'sinatra/json'
 require 'foursquare2'
 
-get '/' do
-  puts ENV['FOURSQUARE_ID']
-  puts ENV['FOURSQUARE_SECRET']
-  
-  client = Foursquare2::Client.new(
+@default_location = 'San Francisco, CA'
+
+def get_client
+  Foursquare2::Client.new(
     :client_id => ENV['FOURSQUARE_ID'],
     :client_secret => ENV['FOURSQUARE_SECRET']
   )
-    
-  @venues_result = client.search_venues(
-    :near => 'San Francisco, CA',
-    :categoryId => '4bf58dd8d48988d17f941735'
-  )
-    
-  @venues = @venues_result["groups"].first["items"]
+end
 
+get '/' do
   erb :index
 end
 
-get '/venue/:venue_id/movies' do
-  client = Foursquare2::Client.new(:client_id => 'AJ5B4KWNYX5MCTWPQJYA5WCTICID404HBRX04BDMM1SDSMNI', :client_secret => 'GPKCG2YQNVTFPJN5OGE5JMCGQ1WOVDKGSPJCYDRPTQTZYOBJ')
+get '/venues' do
+  client = get_client()
+  @venues_result = client.search_venues(
+    :near => params['location'],
+    :categoryId => '4bf58dd8d48988d17f941735'
+  )
+  
+  json @venues_result["groups"].first["items"]
+end
 
+get '/venue/:venue_id/movies' do
+  client = get_client
   @events_result = client.venue_events(params["venue_id"])
 
   json @events_result["events"]["items"]
